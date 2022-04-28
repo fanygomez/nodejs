4ed4228a3ad4e859e40edb52d30cf4ce74a45872
Fany Gómez <fanygomez1894@gmail.com>
2022-04-28 16:22:03 -0600

    Deploy app angular - 28-04-2022

--- 06-web-server/app.js
+++ 06-web-server/app.js
@@ -9,34 +9,35 @@ hbs.registerPartials(__dirname +'/views/partials');
 // servir contenido estatico
 app.use( express.static('public'))
 
-app.get('/', (req, res) =>{
-    res.render('home',{
-        name:'fany',
-        title:'Node'
-    }); 
-});
+// app.get('/', (req, res) =>{
+//     res.render('home',{
+//         name:'fany',
+//         title:'Node'
+//     }); 
+// });
 
-app.get("/hola-mundo", ( req, res) => {
-    res.send("hello world");
-});
-app.get("/generic", ( req, res) => {
-    // res.sendFile(__dirname + '/public/generic.html');
-    res.render('generic',{
-        title:'Generic',
-        name:'Fany'
-    });
-});
+// app.get("/hola-mundo", ( req, res) => {
+//     res.send("hello world");
+// });
+// app.get("/generic", ( req, res) => {
+//     // res.sendFile(__dirname + '/public/generic.html');
+//     res.render('generic',{
+//         title:'Generic',
+//         name:'Fany'
+//     });
+// });
 
-app.get("/elements", ( req, res) => {
-    // res.sendFile(__dirname + '/public/elements.html');
-    res.render('elements',{
-        title:'Elements',
-        name:'Fany'
-    });
-});
+// app.get("/elements", ( req, res) => {
+//     // res.sendFile(__dirname + '/public/elements.html');
+//     res.render('elements',{
+//         title:'Elements',
+//         name:'Fany'
+//     });
+// });
 
 app.get("*", ( req, res) => {
-    res.sendFile(__dirname + "public/error/404.html");
+    // res.sendFile(__dirname + "public/error/404.html");
+    res.sendFile(__dirname + "public/index.html");
 });
 app.listen(port,()=> {
     console.log(`Dev : listening at http://localhots:${port}`)

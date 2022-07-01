4232cfffa8ffe8ff0feb3f992206acc07098c034
Stefhani Gómez <fanygomez1894@gmail.com>
2022-07-01 15:28:59 -0600

    Update
    - Added general search by collection, such as users, category and product.
    - - Added validation for the Products API.
    
    Co-Authored-By: Fany Gómez <11141108+fanygomez@users.noreply.github.com>

--- 07-rest-server/helpers/db-validators.js
+++ 07-rest-server/helpers/db-validators.js
@@ -1,59 +1,44 @@
-const Role = require("../models/role");
-const { User, Category } = require("../models");
+const Role = require('../models/role');
+const { User, Category } = require('../models');
 
-const isValidRole = async (role = "") => {
-  const isRole = await Role.findOne({ role: role });
-  if (!isRole) {
-    throw new Error(`Invalid role`);
-  }
-};
+const isValidRole = async (role = '')=>{
+    const isRole = await Role.findOne({ role: role });
+    if ( !isRole ) {
+        throw new Error(`Invalid role`);
+    }
+}
 
-const existEmail = async (email = "") => {
-  const existEmail = await User.findOne({ email: email });
-  console.log("existEmail", existEmail);
-  if (existEmail) {
-    console.log("entro ");
-    throw new Error("This email exist");
-  }
-};
+const existEmail = async ( email = '') => {
+    const existEmail = await User.findOne({ email: email });
+    console.log("existEmail",existEmail);
+    if ( existEmail) {
+        console.log("entro ");
+        throw new Error("This email exist");
+    }
+}
 
-const existUserById = async (id) => {
-  const existUser = await User.findById(id);
-  if (!existUser) {
-    throw new Error(`El id no existe ${id}`);
-  }
-};
+const existUserById = async( id ) => {
 
-const existCategory = async (category = "") => {
-  const existCategory = await Category.findOne({
-    name: category,
-    status: true,
-  });
-  console.log("existCategory", existCategory);
-  if (existCategory) {
-    throw new Error("This Category exist");
-  }
-};
+    const existUser = await User.findById(id);
+    if ( !existUser ) {
+        throw new Error(`El id no existe ${ id }`);
+    }
+}
 
-const existCategoryById = async (id) => {
-  const existCategory = await Category.findById(id);
-  if (!existCategory) {
-    throw new Error(`El id no existe ${id}`);
-  }
-};
+const existCategory = async ( category = '') => {
+    const existCategory = await Category.findOne({ name: category, status: true });
+    console.log("existCategory",existCategory);
+    if ( existCategory) {
+        throw new Error("This Category exist");
+    }
+}
 
-const existProductById = async (id) => {
-  const existProduct = await Product.findById(id);
-  if (!existProduct) {
-    throw new Error(`El id no existe ${id}`);
-  }
-};
+const existCategoryById = async( id ) => {
 
-module.exports = {
-  isValidRole,
-  existEmail,
-  existUserById,
-  existCategory,
-  existCategoryById,
-  existProductById,
-};
+    const existCategory = await Category.findById(id);
+    if ( !existCategory ) {
+        throw new Error(`El id no existe ${ id }`);
+    }
+}
+
+module.exports = { isValidRole, existEmail, existUserById, existCategory, existCategoryById }
--- 07-rest-server/models/index.js
+++ 07-rest-server/models/index.js
@@ -3,12 +3,10 @@
 const Category = require('./Category');
 const Role = require('./role');
 const User = require('./User');
-const Product = require('./Product');
 const Server = require('./server');
 
 module.exports = {
     Category,
-    Product,
     Role,
     Server,
     User
